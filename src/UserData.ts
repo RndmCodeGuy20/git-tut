1ed4761e5331d2b80fdb78bfe8f1a5c6d2dca8d6
maneshantanu20 <manesg@rknec.edu>
2023-05-05 09:34:45 +0530

    first commit

---
+++ src/UserData.ts
@@ -0,0 +1,11 @@
+export const data = [
+    {
+        id: 1,
+        image_link: 'https://avatars.githubusercontent.com/u/78042851?v=4',
+        name: 'Shantanu Mane',
+        description: 'Full Stack Developer',
+        instagram: 'https://www.instagram.com/shantheman.20/',
+        github: 'https://www.github.com/RndmCodeGuy20',
+        linkedin: 'https://www.linkedin.com/in/maneshantanu.20/',
+    },
+]
